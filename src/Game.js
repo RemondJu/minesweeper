c8909c8f3e19611ad7274413f2f5a5c52099bcc4
Julien Rémond <jremond@ideal-groupe.com>
2022-04-06 17:33:50 +0200

    Refactored Game.js using hooks

--- src/Game.js
+++ src/Game.js
@@ -1,76 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Row from './Row';
 
-class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      gameGrid: [
-        [
-          {x: 0, y: 0, mine: true},
-          {x: 1, y: 0, mine: false},
-          {x: 2, y: 0, mine: true},
-          {x: 3, y: 0, mine: true},
-          {x: 4, y: 0, mine: true},
-          {x: 5, y: 0, mine: true},
-        ],
-        [
-          {x: 0, y: 1, mine: true},
-          {x: 1, y: 1, mine: false},
-          {x: 2, y: 1, mine: false},
-          {x: 3, y: 1, mine: false},
-          {x: 4, y: 1, mine: false},
-          {x: 5, y: 1, mine: false},
-        ],
-        [
-          {x: 0, y: 2, mine: false},
-          {x: 1, y: 2, mine: false},
-          {x: 2, y: 2, mine: true},
-          {x: 3, y: 2, mine: true},
-          {x: 4, y: 2, mine: true},
-          {x: 5, y: 2, mine: true},
-        ],
-        [
-          {x: 0, y: 3, mine: false},
-          {x: 1, y: 3, mine: false},
-          {x: 2, y: 3, mine: false},
-          {x: 3, y: 3, mine: false},
-          {x: 4, y: 3, mine: false},
-          {x: 5, y: 3, mine: false},
-        ],
-        [
-          {x: 0, y: 4, mine: false},
-          {x: 1, y: 4, mine: false},
-          {x: 2, y: 4, mine: true},
-          {x: 3, y: 4, mine: true},
-          {x: 4, y: 4, mine: true},
-          {x: 5, y: 4, mine: true},
-        ]
+export default function Game () {
+  const [gameGrid, setGameGrid] = useState(
+    [
+      [
+        {x: 0, y: 0, mine: true},
+        {x: 1, y: 0, mine: false},
+        {x: 2, y: 0, mine: true},
+        {x: 3, y: 0, mine: true},
+        {x: 4, y: 0, mine: true},
+        {x: 5, y: 0, mine: true},
       ],
-      gameOver: false,
-    }
+      [
+        {x: 0, y: 1, mine: true},
+        {x: 1, y: 1, mine: false},
+        {x: 2, y: 1, mine: false},
+        {x: 3, y: 1, mine: false},
+        {x: 4, y: 1, mine: false},
+        {x: 5, y: 1, mine: false},
+      ],
+      [
+        {x: 0, y: 2, mine: false},
+        {x: 1, y: 2, mine: false},
+        {x: 2, y: 2, mine: true},
+        {x: 3, y: 2, mine: true},
+        {x: 4, y: 2, mine: true},
+        {x: 5, y: 2, mine: true},
+      ],
+      [
+        {x: 0, y: 3, mine: false},
+        {x: 1, y: 3, mine: false},
+        {x: 2, y: 3, mine: false},
+        {x: 3, y: 3, mine: false},
+        {x: 4, y: 3, mine: false},
+        {x: 5, y: 3, mine: false},
+      ],
+      [
+        {x: 0, y: 4, mine: false},
+        {x: 1, y: 4, mine: false},
+        {x: 2, y: 4, mine: true},
+        {x: 3, y: 4, mine: true},
+        {x: 4, y: 4, mine: true},
+        {x: 5, y: 4, mine: true},
+      ]
+    ]
+  )
+  const [gameOver, setGameOver] = useState(false)
+  const handleFailedGame = () => setGameOver(true);
 
-    this.handleFailedGame = this.handleFailedGame.bind(this)
-  }
-
-  handleFailedGame() {
-    this.setState({gameOver: true})
-  }
-
-  render () {
-    return <div className='gameGrid'>
-      {this.state.gameGrid.map((el, index) => 
-        <Row
-          key={'row' + index}
-          squares={el}
-          gameGrid={this.state.gameGrid}
-          handleFailedGame={this.handleFailedGame}
-          gameOver={this.state.gameOver}
-        />
-      )}
-      
-    </div>;
-  }
+  return <div className='gameGrid'>
+    {gameGrid.map((el, index) => 
+      <Row
+        key={'row' + index}
+        squares={el}
+        gameGrid={gameGrid}
+        handleFailedGame={handleFailedGame}
+        gameOver={gameOver}
+      />
+    )}
+  </div>;
 }
-
-export default Game;
