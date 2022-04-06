ca114787629bc78e206bb90eba85a3557c804a9c
Julien Rémond <jremond@ideal-groupe.com>
2022-04-06 17:36:33 +0200

    Refactored Row.js into a functional component

--- src/Row.js
+++ src/Row.js
@@ -1,25 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Square from './Square';
 
-class Row extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render () {
-    return <div className='Row'>
-      {this.props.squares.map(square => 
-        <Square 
-          key={'square' + square.x + square.y} 
-          squareDetails={square}
-          gameGrid={this.props.gameGrid}
-          mine={square.mine}
-          gameOver={this.props.gameOver}
-          handleFailedGame={this.props.handleFailedGame}
-        />
-      )}
-    </div>
-  }
+export default function Row (props) {
+  return <div className='Row'>
+    {props.squares.map(square => 
+      <Square 
+        key={'square' + square.x + square.y} 
+        squareDetails={square}
+        gameGrid={props.gameGrid}
+        mine={square.mine}
+        gameOver={props.gameOver}
+        handleFailedGame={props.handleFailedGame}
+      />
+    )}
+  </div>
 }
-
-export default Row;
