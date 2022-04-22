3c3732d360d465cda90ff11450f78042d8c9d38c
Julien Rémond <jremond@ideal-groupe.com>
2022-04-22 08:41:19 +0200

    setting dynamically generated game grid

--- src/Game.js
+++ src/Game.js
@@ -2,50 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Row from './Row';
 
 export default function Game () {
-  const [gameGrid, setGameGrid] = useState(
-    [
-      [
-        {x: 0, y: 0, mine: false},
-        {x: 1, y: 0, mine: false},
-        {x: 2, y: 0, mine: false},
-        {x: 3, y: 0, mine: false},
-        {x: 4, y: 0, mine: false},
-        {x: 5, y: 0, mine: false},
-      ],
-      [
-        {x: 0, y: 1, mine: false},
-        {x: 1, y: 1, mine: true},
-        {x: 2, y: 1, mine: false},
-        {x: 3, y: 1, mine: false},
-        {x: 4, y: 1, mine: true},
-        {x: 5, y: 1, mine: false},
-      ],
-      [
-        {x: 0, y: 2, mine: true},
-        {x: 1, y: 2, mine: true},
-        {x: 2, y: 2, mine: true},
-        {x: 3, y: 2, mine: true},
-        {x: 4, y: 2, mine: true},
-        {x: 5, y: 2, mine: true},
-      ],
-      [
-        {x: 0, y: 3, mine: false},
-        {x: 1, y: 3, mine: true},
-        {x: 2, y: 3, mine: false},
-        {x: 3, y: 3, mine: false},
-        {x: 4, y: 3, mine: true},
-        {x: 5, y: 3, mine: false},
-      ],
-      [
-        {x: 0, y: 4, mine: false},
-        {x: 1, y: 4, mine: false},
-        {x: 2, y: 4, mine: false},
-        {x: 3, y: 4, mine: false},
-        {x: 4, y: 4, mine: false},
-        {x: 5, y: 4, mine: false},
-      ]
-    ]
-  )
+  const [gameGrid, setGameGrid] = useState([])
   const [gameOver, setGameOver] = useState(false);
   const [gridWidth, setGridWidth] = useState(9);
   const [gridHeight, setGridHeight] = useState(6);
@@ -69,6 +26,7 @@ export default function Game () {
         }
       }
     }
+    setGameGrid(generatedGrid);
   }
 
   if (gameGrid.length) {
