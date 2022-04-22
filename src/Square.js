592cd3e94a87fce3bad580a09bbff823ea67f21f
Julien Rémond <jremond@ideal-groupe.com>
2022-04-22 09:20:28 +0200

    refactored square clicking logic with predefined content

--- src/Square.js
+++ src/Square.js
@@ -2,63 +2,63 @@ import React, { useState, useEffect } from 'react';
 
 export default function Square (props) {
   const [content, setContent] = useState('');
-  const [isClicked, setIsClicked] = useState(false);
+  const [isRevealed, setIsRevealed] = useState(false);
   const [className, setClassName] = useState('Square');
 
   useEffect(() => {
-    if (props.gameOver && !isClicked && content !== 'F') {
+    if (props.gameOver && !isRevealed && content !== 'F') {
       revealContent()
     }
   })
 
   const revealContent = () => {
-    setContent(props.mine ? 'X' : '');
-    setIsClicked(true)
+    setIsRevealed(true)
     setClassName(className + ' clicked failed')
   }
 
-  const showNumberOfNeighborMines = () => {
-    let minX = props.squareDetails.x > 0 ? props.squareDetails.x - 1 : 0
-    let maxX = props.squareDetails.x < props.gameGrid[0].length - 1 ? props.squareDetails.x + 1 : props.squareDetails.x
-    let minY = props.squareDetails.y > 0 ? props.squareDetails.y - 1 : 0
-    let maxY = props.squareDetails.y < props.gameGrid.length - 1 ? props.squareDetails.y + 1 : props.squareDetails.y
+  // const showNumberOfNeighborMines = () => {
+  //   let minX = props.squareDetails.x > 0 ? props.squareDetails.x - 1 : 0
+  //   let maxX = props.squareDetails.x < props.gameGrid[0].length - 1 ? props.squareDetails.x + 1 : props.squareDetails.x
+  //   let minY = props.squareDetails.y > 0 ? props.squareDetails.y - 1 : 0
+  //   let maxY = props.squareDetails.y < props.gameGrid.length - 1 ? props.squareDetails.y + 1 : props.squareDetails.y
 
-    let numberOfNeighborMines = 0
-    props.gameGrid.slice(minY, maxY+1).forEach(function(row) {
-      row.slice(minX, maxX+1).forEach(function(square) { if (square.mine) {numberOfNeighborMines++} })
-    });
-    if (numberOfNeighborMines === 0) {
-      props.revealEmptyNeighorSquares(props.squareDetails.x, props.squareDetails.y)
-    }
-
-    setContent(numberOfNeighborMines > 0 ? numberOfNeighborMines : '');
-    setIsClicked(true);
-    setClassName(className + ' clicked');
-  }
+  //   let numberOfNeighborMines = 0
+  //   props.gameGrid.slice(minY, maxY+1).forEach(function(row) {
+  //     row.slice(minX, maxX+1).forEach(function(square) { if (square.mine) {numberOfNeighborMines++} })
+  //   });
+  //   if (numberOfNeighborMines === 0) {
+  //     props.revealEmptyNeighorSquares(props.squareDetails.x, props.squareDetails.y)
+  //   }
+  // }
 
   const handleClick = (event) => {
     event.preventDefault();
     if (content !== 'F') {
-      if (props.mine) {
+      setIsRevealed(true);
+      if (props.squareDetails.content === 'X') {
         props.handleFailedGame();
       } else {
-        showNumberOfNeighborMines()
-        props.revealEmptyNeighorSquares()
+        setIsRevealed(true);
+        setClassName(className + ' clicked');
       }
     }
   }
 
   const handleRightClick = (event) => {
     event.preventDefault();
-    if (!isClicked) {
+    if (!isRevealed &&  !props.gameOver) {
       setContent(content === 'F' ? '' : 'F')
     }
   }
 
-  return <div
+  if (isRevealed) {
+    return <div className={className}>{props.squareDetails.content}</div>;
+  } else {
+    return <div
       className={className}
       onClick={handleClick}
       onContextMenu={handleRightClick}>
         {content}
     </div>;
+  }
 }
